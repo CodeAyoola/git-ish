a8923d80afb0a4367c39326f5ca9189c93a029be
Ayoola <rajiayoola7@gmail.com>
2022-09-16 19:36:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var database = [
+    {
+        username: "Ayoola",
+        password: "ayoola6882"
+
+
+
+    }
+
+
+];
+
+
+var newsFeed = [
+
+    {
+        username: "Bobby"
+        timeline: "I have been busy all day" 
+    }
+]
